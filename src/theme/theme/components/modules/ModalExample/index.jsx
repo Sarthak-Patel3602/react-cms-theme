aee7f593ca066e3bd7f771da5e32e8ea77ff73a1
Sarthak Patel <sarthak.thoughtmate@gmail.com>
2025-11-23 13:24:19 +0530

    Add Modal Module

---
+++ src/theme/theme/components/modules/ModalExample/index.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+import { logInfo } from '@hubspot/cms-components';
+
+export function Component(props) {
+const {} = props;
+const [showModal, setShowModal] = useState(false);
+
+const MyModal = () => {
+    useEffect(() => {
+      document.body.style.overflowY = "hidden";
+      return () => {
+        document.body.style.overflowY = "scroll";
+      };
+    }, []);
+    return (
+      <>
+        <div
+          className="modal-wrapper"
+          onClick={() => {
+            setShowModal(false);
+          }}
+        ></div>
+        <div className="modal-container">
+          <h1>This is a heading</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex at quo
+            minima vel, iste sed tenetur? Cupiditate suscipit accusantium alias.
+          </p>
+          <button
+            className="modal-btn"
+            onClick={() => {
+              setShowModal(false);
+            }}
+          >
+            Close Button
+          </button>
+        </div>
+      </>
+    );
+  };
+
+//   logInfo(props, 'props');
+return (
+    <>
+<button
+        className="modal-btn"
+        onClick={() => {
+          setShowModal(true);
+        }}
+      >
+        Open Modal
+      </button>
+      <div className="content">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum quisquam
+          sed consequuntur ullam amet, quo corrupti aliquid voluptate ex
+          consectetur, quibusdam, facere cupiditate neque a mollitia? Natus, hic
+          commodi. Ut, eveniet inventore dolorem magni vel laudantium quasi,
+          voluptates fugit numquam est accusantium maiores repellat adipisci
+          labore commodi! Id, ab minus?
+        </p>
+      </div>
+      {showModal && <MyModal />}
+    </>
+);
+}
+
+export { fields } from './fields.jsx';
+
+export const meta = {
+label: 'Modal Example',
+};
