4d1679a6b41d5ccf7b401df7bb2b943d524af8f5
Sarthak Patel <sarthak.thoughtmate@gmail.com>
2025-11-23 13:51:10 +0530

    Solve modal related code

--- src/theme/theme/components/modules/ModalExample/index.jsx
+++ src/theme/theme/components/modules/ModalExample/index.jsx
@@ -1,73 +1,57 @@
 import React, { useEffect, useState } from "react";
-import Style from './style.module.css';
-import { logInfo } from '@hubspot/cms-components';
+import styles from "./style.module.css";
 
-export function Component(props) {
-const {} = props;
-const [showModal, setShowModal] = useState(false);
+function MyModal({ close }) {
+  useEffect(() => {
+    document.body.style.overflowY = "hidden";
+    return () => {
+      document.body.style.overflowY = "scroll";
+    };
+  }, []);
 
-const MyModal = () => {
-    useEffect(() => {
-      document.body.style.overflowY = "hidden";
-      return () => {
-        document.body.style.overflowY = "scroll";
-      };
-    }, []);
-    return (
-     <>
-        <div
-          className={Style.modal_wrapper}
-          onClick={() => {
-            setShowModal(false);
-          }}
-        ></div>
-        <div className={Style.modal_container}>
-          <h1>This is a heading</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex at quo
-            minima vel, iste sed tenetur? Cupiditate suscipit accusantium alias.
-          </p>
-          <button
-            className={Style.modal_btn}
-            onClick={() => {
-              setShowModal(false);
-            }}
-          >
-            Close Button
-          </button>
-        </div>
-      </>
-    );
-  };
-
-//   logInfo(props, 'props');
-return (
+  return (
     <>
-<button
-        className={Style.modal_btn}
-        onClick={() => {
-          setShowModal(true);
-        }}
-      >
-        Open Modal
-      </button>
-      <div className={Style.content}>
+      <div className={styles.modal_wrapper} onClick={close}></div>
+
+      <div className={styles.modal_container}>
+        <h1>This is a heading</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum quisquam
-          sed consequuntur ullam amet, quo corrupti aliquid voluptate ex
-          consectetur, quibusdam, facere cupiditate neque a mollitia? Natus, hic
-          commodi. Ut, eveniet inventore dolorem magni vel laudantium quasi,
-          voluptates fugit numquam est accusantium maiores repellat adipisci
-          labore commodi! Id, ab minus?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
         </p>
+
+        <button className={styles.modal_btn} onClick={close}>
+          Close Button
+        </button>
       </div>
-      {showModal && <MyModal />}
     </>
-);
+  );
 }
 
-export { fields } from './fields.jsx';
+export function Component() {
+  const [showModal, setShowModal] = useState(false);
+
+  const open = () => setShowModal(true);
+  const close = () => setShowModal(false);
+
+  return (
+    <>
+      <button className={styles.modal_btn} onClick={open}>
+        Open Modal
+      </button>
+
+      <div className={styles.content}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+      </div>
+
+      {showModal && <MyModal close={close} />}
+    </>
+  );
+}
+
+export { fields } from "./fields.jsx";
 
 export const meta = {
-label: 'Modal Example',
+  label: "Modal Example",
 };
