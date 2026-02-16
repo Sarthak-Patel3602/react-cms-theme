4c971990c4c6bea81530995e7ddcc72438b80d51
Sarthak Patel <sarthak.thoughtmate@gmail.com>
2026-02-16 22:15:37 +0530

    Add LinkField and update rendering logic in ImageExample component

--- src/theme/theme/components/modules/ImageExample/fields.jsx
+++ src/theme/theme/components/modules/ImageExample/fields.jsx
@@ -3,6 +3,7 @@ import {
   ModuleFields,
   FieldGroup,
   ImageField,
+  LinkField,
 } from '@hubspot/cms-components/fields';
 
 export const fields = (
@@ -27,6 +28,48 @@ export const fields = (
           max_height: 128,
         }}
       />
+      <LinkField
+        name="image_link_field"
+        label="Image Link"
+        supportedTypes={['EXTERNAL', 'CONTENT', 'EMAIL_ADDRESS']}
+        default={{
+          url: { content_id: null, type: 'EXTERNAL', href: '' },
+          open_in_new_tab: false,
+          no_follow: false,
+        }}
+      />
+
+      <RepeatedFieldGroup label="Image Group" name="image_group">
+        <ImageField
+          name="image_field"
+          label="Image"
+          required={false}
+          locked={false}
+          responsive={true}
+          resizable={true}
+          showLoading={false}
+          default={{
+            size_type: 'exact',
+            src: '',
+            alt: 'image-alt-text',
+            loading: 'lazy',
+            width: 128,
+            height: 128,
+            max_width: 128,
+            max_height: 128,
+          }}
+        />
+        <LinkField
+          name="image_link_field"
+          label="Image Link"
+          supportedTypes={['EXTERNAL', 'CONTENT', 'EMAIL_ADDRESS']}
+          default={{
+            url: { content_id: null, type: 'EXTERNAL', href: '' },
+            open_in_new_tab: false,
+            no_follow: false,
+          }}
+        />
+      </RepeatedFieldGroup>
     </FieldGroup>
   </ModuleFields>
 );
--- src/theme/theme/components/modules/ImageExample/index.jsx
+++ src/theme/theme/components/modules/ImageExample/index.jsx
@@ -5,14 +5,54 @@ import RenderImage from '../../components/ImageComponent/imageRenderer.jsx';
 
 export function Component(props) {
   const {
-    groupContent: { image_field },
+    groupContent: { image_field, image_link_field, image_group = [] },
   } = props;
 
   logInfo(props, 'props');
   return (
     <>
+      {/* image with link without repeater */}
       <div className="image-container">
-        <RenderImage imageField={image_field} />
+        <a
+          href={image_link_field?.url?.href_with_scheme || '#'}
+          target={image_link_field.open_in_new_tab ? '_blank' : '_self'}
+          rel={
+            [
+              image_link_field.no_follow ? 'nofollow' : '',
+              image_link_field.sponsored ? 'sponsored' : '',
+              image_link_field.user_generated_content ? 'ugc' : '',
+              image_link_field.rel || '',
+            ]
+              .filter(Boolean)
+              .join(' ') || undefined
+          }
+        >
+          <RenderImage imageField={image_field} />
+        </a>
+      </div>
+
+      {/* image with link with repeater */}
+      <div className="image-container-with-repeater">
+        {image_group.map((image_link_field, index) => {
+          <div className="image" key={index}>
+            <a
+              href={image_link_field?.url?.href_with_scheme || '#'}
+              target={image_link_field.open_in_new_tab ? '_blank' : '_self'}
+              rel={
+                [
+                  image_link_field.no_follow ? 'nofollow' : '',
+                  image_link_field.sponsored ? 'sponsored' : '',
+                  image_link_field.user_generated_content ? 'ugc' : '',
+                  image_link_field.rel || '',
+                ]
+                  .filter(Boolean)
+                  .join(' ') || undefined
+              }
+            >
+              <RenderImage imageField={image_field} />
+            </a>
+          </div>;
+        })}
       </div>
     </>
   );
