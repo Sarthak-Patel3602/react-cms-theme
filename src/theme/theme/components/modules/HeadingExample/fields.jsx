60b623125da7ab7abdde0ca35ac61ab517e170cb
Sarthak Patel <sarthak.thoughtmate@gmail.com>
2026-02-16 21:04:10 +0530

    Refactor HeadingExample component to improve structure and add third heading group handling

--- src/theme/theme/components/modules/HeadingExample/fields.jsx
+++ src/theme/theme/components/modules/HeadingExample/fields.jsx
@@ -1,31 +1,37 @@
 import React from 'react';
 import {
-    ModuleFields,
-    TextField,
-    FieldGroup
+  ModuleFields,
+  TextField,
+  FieldGroup,
 } from '@hubspot/cms-components/fields';
 // import both statement for heading
 import HeadingStyles from '../../components/HeadingComponent/HeadingStyle.jsx';
 import HeadingContent from '../../components/HeadingComponent/HeadingContent.jsx';
 
 export const fields = (
-    <ModuleFields>
-        <FieldGroup
-            label='Heading Group'
-            name='heading_group'
-        >
-            <HeadingContent />
-            <HeadingStyles headingStyleAsDefault='h3' />
-        </FieldGroup>
-        {/* when use multiple heading */}
-        <FieldGroup
-            label='Second Heading Group'
-            name='second_heading_group'
-        >
-            <HeadingContent />
-            <HeadingStyles headingStyleAsDefault='h3' />
-        </FieldGroup>
+  <ModuleFields>
+    <FieldGroup label="Heading Group" name="heading_group">
+      <HeadingContent />
+      <HeadingStyles headingStyleAsDefault="h3" />
+    </FieldGroup>
+    {/* when use multiple heading */}
+    <FieldGroup label="Second Heading Group" name="second_heading_group">
+      <HeadingContent />
+      <HeadingStyles headingStyleAsDefault="h3" />
+    </FieldGroup>
 
-
-    </ModuleFields>
+    {/* Multiple heading component with repeater */}
+    <RepeatedFieldGroup
+      name="third_heading_group"
+      label="Third Heading Group"
+      occurrence={{
+        min: 1,
+        max: 10,
+        default: 1,
+      }}
+    >
+      <HeadingContent />
+      <HeadingStyles headingStyleAsDefault="h3" />
+    </RepeatedFieldGroup>
+  </ModuleFields>
 );
--- src/theme/theme/components/modules/HeadingExample/index.jsx
+++ src/theme/theme/components/modules/HeadingExample/index.jsx
@@ -3,55 +3,68 @@ import { logInfo } from '@hubspot/cms-components';
 // import this statement for heading component
 import HeadingComponent from '../../components/HeadingComponent/HeadingComponent.jsx';
 
-
 export function Component(props) {
-    const {
-        heading_group: {
-            headingAndTextHeadingLevel,
-            headingStyleVariant,
-            headingAndTextHeading,
-            headingStyleColor
-        },
-        // when use multiple heading
-        second_heading_group: {
-                headingAndTextHeadingLevel: headingAndTextHeadingLevelV2,
-                headingStyleVariant: headingStyleVariantV2,
-                headingAndTextHeading: headingAndTextHeadingV2,
-                headingStyleColor: headingStyleColorV2  
-        }
-    } = props;
+  const {
+    heading_group: {
+      headingAndTextHeadingLevel,
+      headingStyleVariant,
+      headingAndTextHeading,
+      headingStyleColor,
+    },
+    // when use multiple heading
+    second_heading_group: {
+      headingAndTextHeadingLevel: headingAndTextHeadingLevelV2,
+      headingStyleVariant: headingStyleVariantV2,
+      headingAndTextHeading: headingAndTextHeadingV2,
+      headingStyleColor: headingStyleColorV2,
+    },
+    third_heading_group = [],
+  } = props;
 
-    logInfo(props, 'props');
-    return (
-        <>
-            <div className="Heading-container">
-                {headingAndTextHeading && (
-                    <HeadingComponent
-                        headingLevel={headingAndTextHeadingLevel}
-                        headingStyleVariant={headingStyleVariant}
-                        heading={headingAndTextHeading}
-                        headingStyleColor={headingStyleColor}
-                    />
-                )}
-            </div>
-            
-            {/* when use multiple heading */}
-            <div className="Second-Heading-container">
-                {headingAndTextHeadingV2 && (
-                    <HeadingComponent
-                        headingLevel={headingAndTextHeadingLevelV2}
-                        headingStyleVariant={headingStyleVariantV2}
-                        heading={headingAndTextHeadingV2}
-                        headingStyleColor={headingStyleColorV2}
-                    />
-                )}
-            </div>
-        </>
-    );
+  logInfo(props, 'props');
+  return (
+    <>
+      <div className="Heading-container">
+        {headingAndTextHeading && (
+          <HeadingComponent
+            headingLevel={headingAndTextHeadingLevel}
+            headingStyleVariant={headingStyleVariant}
+            heading={headingAndTextHeading}
+            headingStyleColor={headingStyleColor}
+          />
+        )}
+      </div>
+
+      {/* when use multiple heading */}
+      <div className="Second-Heading-container">
+        {headingAndTextHeadingV2 && (
+          <HeadingComponent
+            headingLevel={headingAndTextHeadingLevelV2}
+            headingStyleVariant={headingStyleVariantV2}
+            heading={headingAndTextHeadingV2}
+            headingStyleColor={headingStyleColorV2}
+          />
+        )}
+      </div>
+
+      <div className="third-heading-container">
+        {third_heading_group.map((heading, index) => {
+          <div className="heading">
+            <HeadingComponent
+              headingLevel={heading.headingAndTextHeadingLevel}
+              headingStyleVariant={heading.headingStyleVariant}
+              heading={heading.headingAndTextHeading}
+              headingStyleColor={heading.headingStyleColor}
+            />
+          </div>;
+        })}
+      </div>
+    </>
+  );
 }
 
 export { fields } from './fields.jsx';
 
 export const meta = {
-    label: 'Heading Example',
+  label: 'Heading Example',
 };
