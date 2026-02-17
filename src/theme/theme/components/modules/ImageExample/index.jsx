6038cf63e8ad8057cdf1ce1755d380e4197b22ba
Sarthak Patel <sarthak.thoughtmate@gmail.com>
2026-02-17 10:27:16 +0530

    Refactor image group mapping for improved readability and consistency

--- src/theme/theme/components/modules/ImageExample/index.jsx
+++ src/theme/theme/components/modules/ImageExample/index.jsx
@@ -33,27 +33,26 @@ export function Component(props) {
 
       {/* image with link with repeater */}
       <div className="image-container-with-repeater">
-        {image_group.map((image_link_field, index) => {
-          return (
-            <div className="image" key={index}>
-              <a
-                href={image_link_field?.url?.href_with_scheme || '#'}
-                target={image_link_field.open_in_new_tab ? '_blank' : '_self'}
-                rel={
-                  [
-                    image_link_field.no_follow ? 'nofollow' : '',
-                    image_link_field.sponsored ? 'sponsored' : '',
-                    image_link_field.user_generated_content ? 'ugc' : '',
-                    image_link_field.rel || '',
-                  ]
-                    .filter(Boolean)
-                    .join(' ') || undefined
-                }
-              >
-                <RenderImage imageField={image_link_field.image_field} />
-              </a>
-            </div>);
-        })}
+        {image_group.map((item, index) => (
+          <div className="image" key={index}>
+            <a
+              href={item.image_link_field?.url?.href_with_scheme || '#'}
+              target={item.image_link_field.open_in_new_tab ? '_blank' : '_self'}
+              rel={
+                [
+                  item.image_link_field.no_follow ? 'nofollow' : '',
+                  item.image_link_field.sponsored ? 'sponsored' : '',
+                  item.image_link_field.user_generated_content ? 'ugc' : '',
+                  item.image_link_field.rel || '',
+                ]
+                  .filter(Boolean)
+                  .join(' ') || undefined
+              }
+            >
+              <RenderImage imageField={item.image_field} />
+            </a>
+          </div>
+        ))}
       </div>
     </>
   );
